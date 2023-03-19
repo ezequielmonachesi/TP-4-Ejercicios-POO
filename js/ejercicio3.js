d496ec53dffa2e30a04b0e68a01040ce5998ca87
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-19 20:26:17 -0300

    ejercicio3 terminado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,27 +2,32 @@
 propiedades de alto y ancho, mas los métodos necesarios para modificar y
 mostrar sus propiedades, calcular el perímetro y el área */
 
-class rectangulos{
-    constructor(alto, ancho){
-        this.alto = alto,
-        this.ancho = ancho
-    }
-    editarAltura(altura){
-        this.alto = altura;
-    }
-    editarAncho(ancho){
-        this.ancho = ancho;
-    }
-    alto(){
-        this.alto;
-    }
-    ancho(){
-        this.ancho
-    }
-    perimetro(){
-        document.write(`El perímetro del rectangulo es: ${this.alto * this.ancho}`)
-    }
-    area(){
-        document.write(`El perímetro del rectangulo es: ${this.alto * this.ancho / 2}`)
-    }
+class Rectangulos {
+  constructor(alto, ancho) {
+    (this.alto = alto), (this.ancho = ancho);
+  }
+  editar(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+    alert(
+      `Se editaron exitosamente los valores: Altura ${this.alto}, Ancho ${this.ancho}`
+    );
+  }
+  mostrarDatos() {
+    document.write(`El ancho es: ${this.ancho} <br>`);
+    document.write(`La altura es: ${this.alto} <br>`);
+  }
+  perimetro() {
+    document.write(
+      `El perímetro del rectangulo es: ${(this.alto + this.ancho) * 2}<br>`
+    );
+  }
+  area() {
+    document.write(
+      `El área del rectangulo es: ${(this.alto * this.ancho) / 2} <br>`
+    );
+  }
 }
+
+let rectangulo = new Rectangulos(10, 5);
+let cuadrado = new Rectangulos(30, 30);
