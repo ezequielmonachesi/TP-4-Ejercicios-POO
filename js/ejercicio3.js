829db022f899bf290fa20e971e5e01daa0edcf34
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-27 18:44:25 -0300

    Ejercicio3 finalizado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,32 +2,53 @@
 propiedades de alto y ancho, mas los métodos necesarios para modificar y
 mostrar sus propiedades, calcular el perímetro y el área */
 
-class Rectangulos {
+class Rectangulo {
+  #alto
+  #ancho
   constructor(alto, ancho) {
-    (this.alto = alto), (this.ancho = ancho);
+    (this.#alto = alto), (this.#ancho = ancho);
+  }
+  get alto(){
+    return this.#alto;
+  }
+  set alto(nuevaAltura){
+    this.#alto=nuevaAltura;
+  }
+  get ancho(){
+    return this.#ancho;
+  }
+  set ancho(nuevoAncho){
+    this.#ancho=nuevoAncho;
   }
   editar(alto, ancho) {
-    this.alto = alto;
-    this.ancho = ancho;
-    alert(
-      `Se editaron exitosamente los valores: Altura ${this.alto}, Ancho ${this.ancho}`
+    this.#alto = alto;
+    this.#ancho = ancho;
+    document.write(
+      `Se editaron exitosamente los valores del rectángulo: Altura ${this.#alto}, Ancho ${this.#ancho} <br>`
     );
   }
   mostrarDatos() {
-    document.write(`El ancho es: ${this.ancho} <br>`);
-    document.write(`La altura es: ${this.alto} <br>`);
+    document.write(`El ancho del rectángulo es: ${this.#ancho} <br>`);
+    document.write(`La altura del rectángulo es: ${this.#alto} <br>`);
   }
   perimetro() {
     document.write(
-      `El perímetro del rectangulo es: ${(this.alto + this.ancho) * 2}<br>`
+      `El perímetro del rectangulo es: ${(this.#alto + this.#ancho) * 2}<br>`
     );
   }
   area() {
     document.write(
-      `El área del rectangulo es: ${(this.alto * this.ancho) / 2} <br>`
+      `El área del rectangulo es: ${this.#alto * this.#ancho} <br>`
     );
   }
 }
 
-let rectangulo = new Rectangulos(10, 5);
-let cuadrado = new Rectangulos(30, 30);
+let rectangulo = new Rectangulo(10, 5);
+
+rectangulo.mostrarDatos();
+rectangulo.perimetro();
+rectangulo.area();
+rectangulo.editar(20,10);
+rectangulo.mostrarDatos();
+rectangulo.perimetro();
+rectangulo.area();
